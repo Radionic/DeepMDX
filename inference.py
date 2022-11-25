497f5091a006f7cbee5280c39574f9561a94ff23
ArchiMickey <wuchunyat119@gmail.com>
2022-11-25 21:12:15 +0800

    fix import

--- inference.py
+++ inference.py
@@ -7,10 +7,10 @@ import soundfile as sf
 import torch
 from tqdm import tqdm
 
-from lib import dataset
-from lib import nets
-from lib import spec_utils
-from lib import utils
+from src import dataset
+from src import nets
+from src import spec_utils
+from src import utils
 
 
 class Separator(object):
---
+++ src/utils.py
@@ -0,0 +1,39 @@
+import os
+
+import cv2
+import numpy as np
+
+from torch import nn
+
+def _weights_init(m):
+    if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d)):
+        nn.init.normal_(m.weight, 0.0, 0.02)
+    if isinstance(m, nn.BatchNorm2d):
+        nn.init.normal_(m.weight, 0.0, 0.02)
+        nn.init.constant_(m.bias, 0)
+
+
+def imread(filename, flags=cv2.IMREAD_COLOR, dtype=np.uint8):
+    try:
+        n = np.fromfile(filename, dtype)
+        img = cv2.imdecode(n, flags)
+        return img
+    except Exception as e:
+        print(e)
+        return None
+
+
+def imwrite(filename, img, params=None):
+    try:
+        ext = os.path.splitext(filename)[1]
+        result, n = cv2.imencode(ext, img, params)
+
+        if result:
+            with open(filename, mode='w+b') as f:
+                n.tofile(f)
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(e)
+        return False
